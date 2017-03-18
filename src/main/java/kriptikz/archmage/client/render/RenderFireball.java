b531de944cd0ef0e5e109786cbd95147846b730f
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-18 00:22:49 -0500

    Testing out custom texture

--- src/main/java/kriptikz/archmage/client/render/RenderFireball.java
+++ src/main/java/kriptikz/archmage/client/render/RenderFireball.java
@@ -2,15 +2,23 @@ package kriptikz.archmage.client.render;
 
 import kriptikz.archmage.Reference;
 import kriptikz.archmage.spell.SpellFireball;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.VertexBuffer;
 import net.minecraft.client.renderer.entity.Render;
 import net.minecraft.client.renderer.entity.RenderManager;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.fml.client.registry.IRenderFactory;
 
 public class RenderFireball extends Render<SpellFireball>
 {
 	private ResourceLocation texture = new ResourceLocation(Reference.MODID, "textures/entity/projectiles/fireball.png");
+	//private TextureAtlasSprite textureSprite;
 	public static final RenderFactory FACTORY = new RenderFactory();
+	private final float scale = 1;
 
 	protected RenderFireball(RenderManager renderManager)
 	{
@@ -20,8 +28,38 @@ public class RenderFireball extends Render<SpellFireball>
 	@Override
 	public void doRender(SpellFireball entity, double x, double y, double z, float entityYaw, float partialTicks)
 	{
-		//this.bindTexture(texture);
-		super.doRender(entity, x, y, z, entityYaw, partialTicks);
+        GlStateManager.pushMatrix();
+        this.bindEntityTexture(entity);
+        GlStateManager.translate((float)x, (float)y, (float)z);
+        GlStateManager.enableRescaleNormal();
+        GlStateManager.scale(this.scale, this.scale, this.scale);
+        Tessellator tessellator = Tessellator.getInstance();
+        VertexBuffer vertexbuffer = tessellator.getBuffer();
+        GlStateManager.rotate(180.0F - this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+        GlStateManager.rotate((float)(this.renderManager.options.thirdPersonView == 2 ? -1 : 1) * -this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+
+        if (this.renderOutlines)
+        {
+            GlStateManager.enableColorMaterial();
+            GlStateManager.enableOutlineMode(this.getTeamColor(entity));
+        }
+
+        vertexbuffer.begin(7, DefaultVertexFormats.POSITION_TEX_NORMAL);
+        vertexbuffer.pos(-0.5D, -0.25D, 0.0D).tex((double)1, (double)0).normal(0.0F, 1.0F, 0.0F).endVertex();
+        vertexbuffer.pos(0.5D, -0.25D, 0.0D).tex((double)1, (double)1).normal(0.0F, 1.0F, 0.0F).endVertex();
+        vertexbuffer.pos(0.5D, 0.75D, 0.0D).tex((double)1, (double)0).normal(0.0F, 1.0F, 0.0F).endVertex();
+        vertexbuffer.pos(-0.5D, 0.75D, 0.0D).tex((double)0, (double)0).normal(0.0F, 1.0F, 0.0F).endVertex();
+        tessellator.draw();
+
+        if (this.renderOutlines)
+        {
+            GlStateManager.disableOutlineMode();
+            GlStateManager.disableColorMaterial();
+        }
+
+        GlStateManager.disableRescaleNormal();
+        GlStateManager.popMatrix();
+        super.doRender(entity, x, y, z, entityYaw, partialTicks);
 	}
 
 
