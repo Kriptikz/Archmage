df4171d8b90e4bfda150e41319a3817fe36dc718
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-22 21:51:32 -0500

    Added register for TextureSticher and added spawnParticle method for custom particles

--- src/main/java/kriptikz/archmage/proxy/ClientProxy.java
+++ src/main/java/kriptikz/archmage/proxy/ClientProxy.java
@@ -1,11 +1,17 @@
 package kriptikz.archmage.proxy;
 
+import javax.annotation.Nullable;
+
+import kriptikz.archmage.client.TextureStitcher;
+import kriptikz.archmage.client.particle.ParticleIce;
 import kriptikz.archmage.init.ModEntities;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.Particle;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.EnumParticleTypes;
 import net.minecraft.util.IThreadListener;
 import net.minecraft.world.World;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.network.simpleimpl.MessageContext;
 
 public class ClientProxy implements IProxy
@@ -15,6 +21,9 @@ public class ClientProxy implements IProxy
 	@Override
 	public void preInit()
 	{
+		// register texture stitcher
+		MinecraftForge.EVENT_BUS.register(new TextureStitcher());
+		
 		// register entity models
 		ModEntities.registerModels();
 	}
@@ -35,7 +44,23 @@ public class ClientProxy implements IProxy
 	public void spawnParticle(EnumParticleTypes particleType, double xCoord, double yCoord, double zCoord, double xSpeed, double ySpeed,
 			double zSpeed)
 	{
-		MINECRAFT.world.spawnParticle(particleType, xCoord, yCoord, zCoord, xSpeed, ySpeed, zSpeed);	
+		MINECRAFT.world.spawnParticle(particleType, xCoord, yCoord, zCoord, xSpeed, ySpeed, zSpeed);
+	}
+	
+	@Override
+	public void spawnParticle(String particleName, @Nullable int version, double xCoord, double yCoord, double zCoord, double xSpeed, double ySpeed,
+			double zSpeed)
+	{
+		Particle particle;
+		
+		switch(particleName)
+		{
+			case "ice" : particle = new ParticleIce(version, MINECRAFT.world, xCoord, yCoord, zCoord, xSpeed, ySpeed, zSpeed);
+				break;
+			default: particle = null;
+		}
+		
+		MINECRAFT.effectRenderer.addEffect(particle);
 	}
 
 	@Override
