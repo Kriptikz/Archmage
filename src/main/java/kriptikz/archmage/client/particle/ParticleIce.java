f2465469fa83fbdc06e6ee6b97c4a81ae87fc5ab
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-22 21:27:50 -0500

    Added an Ice Particle for Ice Spells

---
+++ src/main/java/kriptikz/archmage/client/particle/ParticleIce.java
@@ -0,0 +1,40 @@
+package kriptikz.archmage.client.particle;
+
+import kriptikz.archmage.Reference;
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.particle.Particle;
+import net.minecraft.client.renderer.texture.TextureAtlasSprite;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+
+public class ParticleIce extends Particle
+{
+	private final ResourceLocation ice1 = new ResourceLocation(Reference.MODID, "particle/ice1");
+	//private final float iceScale;
+	
+	public ParticleIce(int version, World worldIn, double xCoordIn, double yCoordIn, double zCoordIn, double xSpeedIn, double ySpeedIn, double zSpeedIn)
+	{
+		super(worldIn, xCoordIn, yCoordIn, zCoordIn, xSpeedIn, ySpeedIn, zSpeedIn);
+
+        this.motionX = this.motionX * 0.009999999776482582D + xSpeedIn;
+        this.motionY = this.motionY * 0.009999999776482582D + ySpeedIn;
+        this.motionZ = this.motionZ * 0.009999999776482582D + zSpeedIn;
+        this.posX += (double)((this.rand.nextFloat() - this.rand.nextFloat()) * 0.05F);
+        this.posY += (double)((this.rand.nextFloat() - this.rand.nextFloat()) * 0.05F);
+        this.posZ += (double)((this.rand.nextFloat() - this.rand.nextFloat()) * 0.05F);
+        this.particleScale = this.particleScale * 0.5f;
+        this.particleRed = 1.0F;
+        this.particleGreen = 1.0F;
+        this.particleBlue = 1.0F;
+        this.particleMaxAge = (int)(8.0D / (Math.random() * 0.8D + 0.2D)) + 4;
+        
+        TextureAtlasSprite texture = Minecraft.getMinecraft().getTextureMapBlocks().getAtlasSprite(ice1.toString());
+        this.setParticleTexture(texture);
+	}
+	
+	@Override
+	public int getFXLayer()
+	{
+		return 1;
+	}
+}
