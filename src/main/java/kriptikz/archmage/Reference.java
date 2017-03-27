67a7a1b9d2c122ac31b46926b1e24a7438491397
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-27 12:18:44 -0500

    added TRUE, FALSE constants as integer values for spell/status effect integer arrays

--- src/main/java/kriptikz/archmage/Reference.java
+++ src/main/java/kriptikz/archmage/Reference.java
@@ -1,7 +1,15 @@
 package kriptikz.archmage;
 
+/**
+ * A class of static finals for referencing throughout code.
+ * @author kriptikz
+ *
+ */
 public class Reference
 {
+	public static final int TRUE = 1;
+	public static final int FALSE = 0;
+	
 	public static final String MODID = "archmage";
 	public static final String VERSION = "0.0.1";
 	public static final String NAME = "Archmage";
