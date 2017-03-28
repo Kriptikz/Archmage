34be9b96ae40b9dbc64fba38d804a7a34997bc72
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-28 12:11:07 -0500

    updated for renames

--- src/main/java/kriptikz/archmage/handler/AttachCapabilityHandler.java
+++ src/main/java/kriptikz/archmage/handler/AttachCapabilityHandler.java
@@ -3,7 +3,7 @@ package kriptikz.archmage.handler;
 import kriptikz.archmage.Reference;
 import kriptikz.archmage.capability.archmagelevel.ArchmageLevelProvider;
 import kriptikz.archmage.capability.mana.ManaProvider;
-import kriptikz.archmage.capability.spelldata.SpellDataProvider;
+import kriptikz.archmage.capability.spelldata.SpellsProvider;
 import kriptikz.archmage.capability.statuseffects.StatusEffectsProvider;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityLivingBase;
@@ -22,7 +22,7 @@ public class AttachCapabilityHandler
 {
 	public static final ResourceLocation ARCHMAGE_LEVEL = new ResourceLocation(Reference.MODID, "archmage_level");
 	public static final ResourceLocation MANA = new ResourceLocation(Reference.MODID, "mana");
-	public static final ResourceLocation SPELL_DATA = new ResourceLocation(Reference.MODID, "spell_data");
+	public static final ResourceLocation SPELLS = new ResourceLocation(Reference.MODID, "spells");
 	public static final ResourceLocation STATUS_EFFECTS = new ResourceLocation(Reference.MODID, "status_effects");
 	
 	@SubscribeEvent
@@ -32,7 +32,7 @@ public class AttachCapabilityHandler
 		{
 			event.addCapability(ARCHMAGE_LEVEL, new ArchmageLevelProvider());
 			event.addCapability(MANA, new ManaProvider());
-			event.addCapability(SPELL_DATA, new SpellDataProvider());
+			event.addCapability(SPELLS, new SpellsProvider());
 		}
 		
 		if (event.getObject() instanceof EntityLivingBase)
