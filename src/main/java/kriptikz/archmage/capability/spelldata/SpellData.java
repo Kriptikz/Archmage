e09a9054b4ca7a167ec4e2e12b48a5f6fe04b84d
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-27 23:00:09 -0500

    removed methods handled in Spell class

--- src/main/java/kriptikz/archmage/capability/spelldata/SpellData.java
+++ src/main/java/kriptikz/archmage/capability/spelldata/SpellData.java
@@ -1,6 +1,5 @@
 package kriptikz.archmage.capability.spelldata;
 
-import kriptikz.archmage.Reference;
 import kriptikz.archmage.spell.TeleportDestination;
 
 public class SpellData implements ISpellData
@@ -8,7 +7,7 @@ public class SpellData implements ISpellData
 	/**
 	 * The players currently selected spell.
 	 */
-	private EnumSpellId selectedSpell = EnumSpellId.FIREBALL;
+	private SpellName selectedSpell = SpellName.FIREBALL;
 	
 	/**
 	 * The players teleport destination for the teleport spell.
@@ -19,12 +18,13 @@ public class SpellData implements ISpellData
 	 * An array to temporarily set to a spell array in order to set/get the spell arrays data.
 	 * Index: [0] = IS_LEARNED, [1] = level, [2] = xp, [3] = levelMaxXp, [4] = shouldGainXp
 	 */
-	private int[] temp = new int[5];
+	//private int[] temp = new int[5];
+	//private Spell temp = null;
 	
 	/**
-	 * Spell array to hold spell data.
-	 * Index: [0] = isLearned, [1] = level, [2] = xp, [3] = levelMaxXp, [4] = shouldGainXp
+	 * {@link Spell} object to hold spell data.
 	 */
+	/*
 	private int[] spellFireball = {0, 1, 0, 100, 1},
 				  spellStunEntity = {0, 1, 0, 100, 1},
 				  spellSelfHeal = {0, 1, 0, 100, 1},
@@ -35,159 +35,65 @@ public class SpellData implements ISpellData
 				  spellLevitate = {0, 1, 0, 100, 1},
 				  spellWarp = {0, 1, 0, 100, 1},
 				  spellTeleport = {0, 1, 0, 100, 1};
+	*/
+	
+	/**
+	 * {@link Spell} object array of spells to hold individual spell data.
+	 */
+	private Spell[] spells = new Spell[] { 
+			new Spell(SpellName.FIREBALL),
+			new Spell(SpellName.STUN),
+			new Spell(SpellName.SELF_HEAL),
+			new Spell(SpellName.HEAL_PROJECTILE),
+			new Spell(SpellName.DIZZIFY),
+			new Spell(SpellName.SLOW),
+			new Spell(SpellName.DUMMY_CLONE),
+			new Spell(SpellName.LEVITATE),
+			new Spell(SpellName.WARP),
+			new Spell(SpellName.TELEPORT)};
 	
 	@Override
 	public void setSelectedSpell(int newSelectedSpell)
 	{
-		for (EnumSpellId spellId : EnumSpellId.values())
+		for (SpellName spell : SpellName.values())
 		{
-			if (spellId.getId() == newSelectedSpell)
+			if (spell.getId() == newSelectedSpell)
 			{
-				this.selectedSpell = spellId;
+				this.selectedSpell = spell;
 			}
 		}
 	}
 
 	@Override
-	public void setSelectedSpell(EnumSpellId newSelectedSpell)
+	public void setSelectedSpell(SpellName newSelectedSpell)
 	{
 		this.selectedSpell = newSelectedSpell;
 	}
 
 	@Override
-	public EnumSpellId getSelectedSpell()
+	public SpellName getSelectedSpell()
 	{
 		return this.selectedSpell;
 	}
-
+	
 	@Override
-	public void setSpellIsLearned(EnumSpellId spellId, int newIS_LEARNED)
+	public Spell[] getSpells()
 	{
-		setTemp(spellId);
-		
-		this.temp[EnumSpellData.IS_LEARNED.getIndexValue()] = newIS_LEARNED;
+		return this.spells;
 	}
-
+	
 	@Override
-	public int getSpellIsLearned(EnumSpellId spellId)
+	public Spell getSpell(SpellName spellName)
 	{
-		setTemp(spellId);
-		
-		return this.temp[EnumSpellData.IS_LEARNED.getIndexValue()];
-	}
-
-	@Override
-	public void setSpellLevel(EnumSpellId spellId, int newSpellLevel)
-	{
-		setTemp(spellId);
-		
-		if (this.temp[EnumSpellData.LEVEL.getIndexValue()] >= 10)
+		for (Spell spell : spells)
 		{
-			this.temp[EnumSpellData.LEVEL.getIndexValue()] = 10;
-			this.temp[EnumSpellData.SHOULD_GAIN_XP.getIndexValue()] = Reference.FALSE;
-		}
-		else if (this.temp[EnumSpellData.LEVEL.getIndexValue()] < 1)
-		{
-			this.temp[EnumSpellData.LEVEL.getIndexValue()] = 1;
-			this.temp[EnumSpellData.SHOULD_GAIN_XP.getIndexValue()] = Reference.TRUE;
-		}
-		else
-		{
-			this.temp[EnumSpellData.LEVEL.getIndexValue()] = newSpellLevel;
-			this.temp[EnumSpellData.SHOULD_GAIN_XP.getIndexValue()] = Reference.TRUE;
+			if (spell.getName() == spellName.toString())
+			{
+				return spell;
+			}
 		}
 		
-	}
-
-	@Override
-	public int getSpellLevel(EnumSpellId spellId)
-	{
-		setTemp(spellId);
-		
-		return this.temp[EnumSpellData.LEVEL.getIndexValue()];
-	}
-
-	@Override
-	public void setSpellXp(EnumSpellId spellId, int newSpellXp)
-	{
-		setTemp(spellId);
-		
-		this.temp[EnumSpellData.XP.getIndexValue()] = newSpellXp;
-	}
-
-	@Override
-	public int getSpellXp(EnumSpellId spellId)
-	{
-		setTemp(spellId);
-		
-		return this.temp[EnumSpellData.XP.getIndexValue()];
-	}
-
-	@Override
-	public void setSpellLevelMaxXp(EnumSpellId spellId, int newLevelMaxXp)
-	{
-		setTemp(spellId);
-		
-		this.temp[EnumSpellData.LEVEL_MAX_XP.getIndexValue()] = newLevelMaxXp;
-	}
-
-	@Override
-	public int getSpellLevelMaxXp(EnumSpellId spellId)
-	{
-		setTemp(spellId);
-		
-		return this.temp[EnumSpellData.LEVEL_MAX_XP.getIndexValue()];
-	}
-
-	@Override
-	public void setShouldGainXp(EnumSpellId spellId, int newShouldGainXp)
-	{
-		setTemp(spellId);
-		
-		if (newShouldGainXp == Reference.TRUE)
-		{
-			this.temp[EnumSpellData.SHOULD_GAIN_XP.getIndexValue()] = Reference.TRUE;
-		}
-		else
-		{
-			this.temp[EnumSpellData.SHOULD_GAIN_XP.getIndexValue()] = Reference.FALSE;
-		}
-	}
-
-	@Override
-	public int getShouldGainXp(EnumSpellId spellId)
-	{
-		setTemp(spellId);
-		
-		return this.temp[EnumSpellData.SHOULD_GAIN_XP.getIndexValue()];
-	}
-
-	@Override
-	public void setSpellFromId(EnumSpellId spellId, int[] spell)
-	{
-		setTemp(spellId);
-		
-		this.temp = spell;
-	}
-
-	@Override
-	public int[] getSpellFromId(EnumSpellId spellId)
-	{
-		switch(spellId)
-		{
-			case FIREBALL: return spellFireball;
-			case STUN: return this.spellStunEntity;
-			case SELF_HEAL: return this.spellSelfHeal;
-			case HEAL_PROJECTILE: return this.spellHealProjectile;
-			case DIZZIFY: return this.spellDizzify;
-			case SLOW: return this.spellSlowEntity;
-			case DUMMY_CLONE: return this.spellDummyClone;
-			case LEVITATE: return this.spellLevitate;
-			case WARP: return this.spellWarp;
-			case TELEPORT: return this.spellTeleport;
-			
-			default: return null;
-		}
+		return null;
 	}
 	
 	@Override
@@ -200,15 +106,5 @@ public class SpellData implements ISpellData
 	public TeleportDestination getTeleportDestination()
 	{
 		return this.teleportDestination;
-	}
-	
-	/**
-	 * Set the temp array to a spell array for getting/setting spell array data.
-	 * 
-	 * @param spellId The id of the spell
-	 */
-	private void setTemp(EnumSpellId spellId)
-	{
-		this.temp = getSpellFromId(spellId);
 	}
 }
