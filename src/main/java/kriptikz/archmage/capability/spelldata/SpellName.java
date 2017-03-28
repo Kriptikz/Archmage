cf713de1093ab35830df5e14158ee3d92a812786
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-27 22:56:52 -0500

    changed EnumSpellId to SpellName to better reflect what it is

--- src/main/java/kriptikz/archmage/capability/spelldata/SpellName.java
+++ src/main/java/kriptikz/archmage/capability/spelldata/SpellName.java
@@ -1,23 +1,28 @@
 package kriptikz.archmage.capability.spelldata;
 
 /**
- * Associates each spell with an integer value used for referencing each spell by an Id.
+ * Associates each spell with a name and integer value used for referencing each spell.
  * 
  * @author kriptikz
  *
  */
-public enum EnumSpellId
+public enum SpellName
 {
 	FIREBALL(0), STUN(1), SELF_HEAL(2), HEAL_PROJECTILE(3), DIZZIFY(4), SLOW(5),
 	DUMMY_CLONE(6), LEVITATE(7), WARP(8), TELEPORT(9);
 	
 	private int id;
 	
-	EnumSpellId(int id)
+	SpellName(int id)
 	{
 		this.id = id;
 	}
 	
+	/**
+	 * Get the Integer Id of the spell.
+	 * 
+	 * @return id
+	 */
 	public int getId()
 	{
 		return this.id;
