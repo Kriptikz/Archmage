c30b257492ce5f3510a3dc3f95b350e92feabc32
Kriptikz <Kriptikz@users.noreply.github.com>
2017-03-31 12:42:28 -0500

    implemented EnumStatusEffects and StatusEffectData into this.
    StatusEffectData now handles individual status effect data as well as
    getting/setting that data. EnumStatusEffects is a rename from
    EnumStatusEffectId

--- src/main/java/kriptikz/archmage/capability/statuseffects/IStatusEffects.java
+++ src/main/java/kriptikz/archmage/capability/statuseffects/IStatusEffects.java
@@ -7,84 +7,19 @@ package kriptikz.archmage.capability.statuseffects;
  *
  */
 public interface IStatusEffects
-{
+{	
 	/**
-	 * Set whether the status effect is active.
+	 * Get the status effect data from a status effect enum {@link EnumStatusEffects}.
 	 * 
-	 * @param effectId The id of the status effect
-	 * @param newisActive The new isActive value
+	 * @param effect The enum of the status effect
+	 * @return The {@link StatusEffectData} of the status effect
 	 */
-	public void setIsActive(EnumStatusEffectId effectId, int newisActive);
+	public StatusEffectData getStatusEffectData(EnumStatusEffects effect);
 	
 	/**
-	 * Get whether the status effect is active.
+	 * Get the {@link StatusEffectData} object array from {@link StatusEffects}.
 	 * 
-	 * @param effectId The id of the status effect.
-	 * @return Whether the status effect is active.
+	 * @return
 	 */
-	public int getIsActive(EnumStatusEffectId effectId);
-	
-	/**
-	 * Set the amount of ticks the status effect has been applied for.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @param newTicks The amount of ticks the status effect has been applied for
-	 */
-	public void setTicks(EnumStatusEffectId effectId, int newTicks);
-	
-	/**
-	 * Get the amount of ticks the status effect has been applied for.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @return The amount of ticks the status effect has been applied for
-	 */
-	public int getTicks(EnumStatusEffectId effectId);
-	
-	/**
-	 * Set the duration the status effect should be applied for.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @param newDuration The new duration of the status effect
-	 */
-	public void setDuration(EnumStatusEffectId effectId, int newDuration);
-	
-	/**
-	 * Get the duration the staus effect should applied for.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @return The new duration the status effect should be applied for
-	 */
-	public int getDuration(EnumStatusEffectId effectId);
-	
-	/**
-	 * Set the amp of the status effect.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @param newAmp The new amp of the status effect
-	 */
-	public void setAmp(EnumStatusEffectId effectId, int newAmp);
-	
-	/**
-	 * Get the amp of the status effect.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @return The amp of the status effect
-	 */
-	public int getAmp(EnumStatusEffectId effectId);
-	
-	/**
-	 * Set status effect data from status effect array, by id.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @param statusEffect The array of the new status effect data
-	 */
-	public void setStatusEffectFromId(EnumStatusEffectId effectId, int[] statusEffect);
-	
-	/**
-	 * Get the status effect data from status effect array, by id.
-	 * 
-	 * @param effectId The id of the status effect
-	 * @return The integer array of the status effect data
-	 */
-	public int[] getStatusEffectFromId(EnumStatusEffectId effectId);
+	public StatusEffectData[] getStatusEffects();
 }
--- src/main/java/kriptikz/archmage/capability/statuseffects/StatusEffects.java
+++ src/main/java/kriptikz/archmage/capability/statuseffects/StatusEffects.java
@@ -1,6 +1,5 @@
 package kriptikz.archmage.capability.statuseffects;
 
-import kriptikz.archmage.Reference;
 
 /**
  * Default implementation of {@link IStatusEffects} capability.
@@ -10,116 +9,30 @@ import kriptikz.archmage.Reference;
 public class StatusEffects implements IStatusEffects
 {	
 	/**
-	 * An array to temporarily set to a status effects array in order to get/set status effect data.
-	 * Index: [0] = isActive, [1] = ticks, [2] = duration, [3] = amp
+	 * {@link StatusEffectData} object array of status effects to hold individual status effect data.
 	 */
-	private int[] temp = new int[4];
-	
-	/**
-	 * The following variables are arrays to  hold status effect data.
-	 * Index: [0] = isActive, [1] = ticks, [2] = duration, [3] = amp
-	 */
-	private int[] stunned = {0, 0, 0, 0};
-	private int[] levitating = {0, 0, 0, 0};
+	private StatusEffectData[] statusEffects = new StatusEffectData[] {
+			new StatusEffectData(EnumStatusEffects.LEVITATING),
+			new StatusEffectData(EnumStatusEffects.STUNNED)
+	};
 
 	@Override
-	public void setIsActive(EnumStatusEffectId effectId, int newIsActive)
+	public StatusEffectData getStatusEffectData(EnumStatusEffects effect)
 	{
-		setTemp(effectId);
-		
-		if (newIsActive == Reference.TRUE)
+		for (StatusEffectData statusEffectData : statusEffects)
 		{
-			this.temp[EnumStatusEffectData.IS_ACTIVE.getIndexValue()] = Reference.TRUE;
+			if (statusEffectData.getId() == effect.getId())
+			{
+				return statusEffectData;
+			}
 		}
-		else
-		{
-			this.temp[EnumStatusEffectData.IS_ACTIVE.getIndexValue()] = Reference.FALSE;
-		}
+		
+		return null;
 	}
 	
 	@Override
-	public int getIsActive(EnumStatusEffectId effectId)
+	public StatusEffectData[] getStatusEffects()
 	{
-		setTemp(effectId);
-		
-		return this.temp[EnumStatusEffectData.IS_ACTIVE.getIndexValue()];
-	}
-	
-	@Override
-	public void setTicks(EnumStatusEffectId effectId, int newTicks)
-	{
-		setTemp(effectId);
-		
-		this.temp[EnumStatusEffectData.TICKS.getIndexValue()] = newTicks;
-	}
-
-	@Override
-	public int getTicks(EnumStatusEffectId effectId)
-	{
-		setTemp(effectId);
-		
-		return this.temp[EnumStatusEffectData.TICKS.getIndexValue()];
-	}
-
-	@Override
-	public void setDuration(EnumStatusEffectId effectId, int newDuration)
-	{
-		setTemp(effectId);
-		
-		this.temp[EnumStatusEffectData.DURATION.getIndexValue()] = newDuration;
-	}
-
-	@Override
-	public int getDuration(EnumStatusEffectId effectId)
-	{
-		setTemp(effectId);
-		
-		return this.temp[EnumStatusEffectData.DURATION.getIndexValue()];
-	}
-
-	@Override
-	public void setAmp(EnumStatusEffectId effectId, int newAmp)
-	{
-		setTemp(effectId);
-		
-		this.temp[EnumStatusEffectData.AMP.getIndexValue()] = newAmp;
-	}
-
-	@Override
-	public int getAmp(EnumStatusEffectId effectId)
-	{
-		setTemp(effectId);
-		
-		return this.temp[EnumStatusEffectData.AMP.getIndexValue()];
-	}
-	
-	@Override
-	public void setStatusEffectFromId(EnumStatusEffectId effectId, int[] statusEffect)
-	{
-		setTemp(effectId);
-		
-		this.temp = statusEffect;
-	}
-
-	@Override
-	public int[] getStatusEffectFromId(EnumStatusEffectId effectId)
-	{
-		switch(effectId)
-		{
-			case STUNNED: return this.stunned;
-			case LEVITATING: return this.levitating;
-			
-			default: return null;
-		}
-	}
-	
-	/**
-	 * Set the temp array to a status effect array, for get/set status effect data.
-	 * 
-	 * @param statusEffectId The id of the status effect
-	 */
-	public void setTemp(EnumStatusEffectId effectId)
-	{
-		this.temp = getStatusEffectFromId(effectId);
+		return this.statusEffects;
 	}
 }
